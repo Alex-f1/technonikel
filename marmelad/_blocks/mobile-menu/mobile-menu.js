13a516b60beb6e2a9b74c7be4c9fade4580790e5
Alex <skyriver.f1@gmail.com>
2023-02-06 12:24:20 +0500

    fix mobile menu & more

--- marmelad/_blocks/mobile-menu/mobile-menu.js
+++ marmelad/_blocks/mobile-menu/mobile-menu.js
@@ -1,12 +1,17 @@
 function mobileMenu() {
 
-
   const buttonBurger = document.querySelector(".js-button-burger");
   const body = document.querySelector("body");
+  const topPanel = document.querySelector(".top-panel");
   const mobileMenu = document.querySelector(".mobile-menu");
+  const mobileMenuHead = document.querySelector(".mobile-menu__head");
   const mobileMenuName = document.querySelector(".mobile-menu__name");
   const mobileMenuBack = document.querySelector(".mobile-menu__back");
   const mobileMenuCatalogItem = document.querySelectorAll(".mobile-menu .catalog > li");
+  const mobileMenuCatalog = document.querySelector(".mobile-menu .catalog");
+  const mobileMenuMenu = document.querySelector(".mobile-menu .menu");
+  const buttonCatalog = document.querySelector(".js-button-catalog");
+  const buttonMenu = document.querySelector(".js-button-menu");
 
   buttonBurger.addEventListener("click", function (event) {
     event.currentTarget.classList.toggle("_is-active");
@@ -18,7 +23,6 @@ function mobileMenu() {
 
   mobileMenuCatalogItem.forEach(function (element, index) {
     if (element.children[1]) {
-      // console.log(element);
       let createSubMenuTrigger = document.createElement("span");
       element.classList.value = "sub-menu";
       element.appendChild(createSubMenuTrigger);
@@ -30,8 +34,6 @@ function mobileMenu() {
         element.parentElement.classList.add("_is-triggered");
         element.parentElement.offsetParent.classList.add("_is-triggered");
         const getText = event.target.parentNode.querySelector("a span").firstChild.nodeValue;
-
-        // console.log(getText);
 
         mobileMenuName.innerHTML = getText;
       });
@@ -46,6 +48,29 @@ function mobileMenu() {
       element.parentElement.offsetParent.classList.remove("_is-triggered");
     });
   });
+
+  buttonCatalog.addEventListener("click", function(event) {
+    event.currentTarget.classList.add("_is-active");
+    buttonMenu.classList.remove("_is-active");
+    mobileMenuMenu.classList.remove("_is-show");
+    mobileMenuCatalog.classList.remove("_is-show-menu");
+    mobileMenuHead.classList.remove("_is-show-menu");
+  });
+
+  buttonMenu.addEventListener("click", function(event) {
+    event.currentTarget.classList.add("_is-active");
+    buttonCatalog.classList.remove("_is-active");
+    mobileMenuMenu.classList.add("_is-show");
+    mobileMenuCatalog.classList.add("_is-show-menu");
+    mobileMenuHead.classList.add("_is-show-menu");
+    mobileMenuName.innerHTML = "Меню";
+    mobileMenuCatalogItem.forEach(function (element, index) {
+      element.classList.remove("_is-active");
+      element.parentElement.classList.remove("_is-triggered");
+      element.parentElement.offsetParent.classList.remove("_is-triggered");
+    });
+  });
+
 }
 
 if (window.matchMedia("(max-width: 767px)").matches) {

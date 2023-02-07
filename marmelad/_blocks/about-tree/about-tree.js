33781707b148f50b456b01abefcaab3eb3c8767f
Alex <skyriver.f1@gmail.com>
2023-02-07 14:32:00 +0500

    fast fix

--- marmelad/_blocks/about-tree/about-tree.js
+++ marmelad/_blocks/about-tree/about-tree.js
@@ -1,12 +1,18 @@
-var swiper = new Swiper(".js-about-tree-slider", {
-  slidesPerView: "auto",
-  spaceBetween: 25,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    hide: true,
-  },
-});
+function initAboutTreeSlider() {
+  const aboutTreeSlider = new Swiper(".js-about-tree-slider", {
+    slidesPerView: "auto",
+    spaceBetween: 25,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+  });
+}
+
+if (document.querySelectorAll(".js-about-tree-slider").length) {
+  initAboutTreeSlider();
+}

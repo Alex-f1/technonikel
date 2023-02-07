6f61ca6c419d14be4c120bb269c62f3bb2e37af2
Alex <skyriver.f1@gmail.com>
2023-02-07 14:27:36 +0500

    added about tree & cards

---
+++ marmelad/_blocks/about-tree/about-tree.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".js-about-tree-slider", {
+  slidesPerView: "auto",
+  spaceBetween: 25,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});

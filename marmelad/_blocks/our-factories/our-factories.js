ad2590ae9dabb4f64392e2cee1ac9e61fee393cf
Alex <skyriver.f1@gmail.com>
2023-02-07 22:31:33 +0500

    added map block

---
+++ marmelad/_blocks/our-factories/our-factories.js
@@ -0,0 +1,8 @@
+var cardOurFactoriesSlider = new Swiper(".js-card-our-factories-slider", {
+  effect: "fade",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});

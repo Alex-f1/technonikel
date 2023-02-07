5fcfbeb69f487275d950d06fca43818a9e9b0620
Alex <skyriver.f1@gmail.com>
2023-02-08 00:34:22 +0500

    added events on tab buttons & close card

--- marmelad/_blocks/our-factories/our-factories.js
+++ marmelad/_blocks/our-factories/our-factories.js
@@ -1,4 +1,4 @@
-var cardOurFactoriesSlider = new Swiper(".js-card-our-factories-slider", {
+const cardOurFactoriesSlider = new Swiper(".js-card-our-factories-slider", {
   effect: "fade",
   spaceBetween: 30,
   pagination: {
@@ -6,3 +6,43 @@ var cardOurFactoriesSlider = new Swiper(".js-card-our-factories-slider", {
     clickable: true,
   },
 });
+
+function ourFactoriesInfo() {
+  const buttonTab = document.querySelectorAll(".js-button-tab");
+  const ourFactoriesIconLocation = document.querySelectorAll(".our-factories__icon-location");
+  const ourFactoriesCards = document.querySelector(".js-our-factories-cards");
+  const buttonClose = document.querySelector(".js-button-close");
+
+
+  for (let i = 0; i < buttonTab.length; i++) {
+    buttonTab[i].addEventListener("click", (event) => {
+
+      ourFactoriesCards.classList.add("_is-show")
+
+      let buttonTabChildren = event.target.parentElement.children;
+      for (let b = 0; b < buttonTabChildren.length; b++) {
+        buttonTabChildren[b].classList.remove("_is-active");
+      }
+      
+      buttonTab[i].classList.add("_is-active");
+
+      let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+
+      for (let l = 0; l < tabContentChildren.length; l++) {
+        tabContentChildren[l].classList.remove("_is-active-location");
+      }
+
+      ourFactoriesIconLocation[i].classList.add("_is-active-location");
+
+    });
+  }
+
+  buttonClose.addEventListener("click", function (event) {
+    console.log(event);
+    console.log(event.target.offsetParent.offsetParent);
+    event.target.offsetParent.offsetParent.classList.remove("_is-show")
+  });
+  
+}
+
+ourFactoriesInfo();

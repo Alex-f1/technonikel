9fcf054e7bc6688b402913f856c8a6169d45895d
Alex <skyriver.f1@gmail.com>
2023-02-17 00:04:27 +0500

    property add class selector for pic

--- marmelad/js/app.js
+++ marmelad/js/app.js
@@ -25,4 +25,23 @@ if (window.NodeList && !NodeList.prototype.forEach) {
  * Scripts
  * ========================================================================== */
 
+function propertyCard() {
+  const propertyBlock = document.querySelectorAll(".property");
+  
+  propertyBlock.forEach(function (element) {
+    const findLabel = element.querySelectorAll(".property__label");
+  
+    findLabel.forEach(function (label) {
+      const findImg = label.querySelectorAll(".property__img");
+      if (findImg.length) {
+        findImg[0].offsetParent.parentElement.classList.add("property__labels--pic");
+      }
+    });
+  });
+}
+
+if (document.querySelectorAll(".property").length) {
+  propertyCard();
+}
+
 //=require ../_blocks/**/*.js

25f0958c3f5ee2f224b92e0c4a98e5f00218630c
Alex <skyriver.f1@gmail.com>
2023-03-17 01:18:29 +0500

    added articles 2 page & more

--- marmelad/_blocks/content-block/content-block.js
+++ marmelad/_blocks/content-block/content-block.js
@@ -1,6 +1,10 @@
-if (document.querySelectorAll(".js-content-block-sidebar").length) {
-  document.querySelector(".js-content-block-sidebar")
-    .addEventListener("click", function () {
-      this.classList.toggle("_is-show");
-    });
+if (window.matchMedia("(max-width: 1023px)").matches) {
+  if (document.querySelectorAll(".js-content-block-sidebar").length) {
+    document.querySelector(".js-content-block-sidebar")
+      .addEventListener("click", function () {
+        this.classList.toggle("_is-show");
+      });
+  }
 }
+
+

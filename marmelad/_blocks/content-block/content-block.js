c0062dd9c1b9ee4f7805a455869df2b623fa0a13
Alex <skyriver.f1@gmail.com>
2023-03-17 18:00:43 +0500

    added article 2 page & sidebar fix

--- marmelad/_blocks/content-block/content-block.js
+++ marmelad/_blocks/content-block/content-block.js
@@ -7,4 +7,24 @@ if (window.matchMedia("(max-width: 1023px)").matches) {
   }
 }
 
+if (document.querySelectorAll(".js-content-block-sidebar").length) {
+  const sidebarNav = document.querySelectorAll(".js-content-block-sidebar ul li");
+
+  sidebarNav.forEach(function (element, index) {
+    if (element.children[1]) {
+      let createSubMenuTrigger = document.createElement("span");
+      element.classList.value = "sub-menu";
+      element.appendChild(createSubMenuTrigger);
+      element.children[2].classList.value = "sub-menu-trigger";
+      let subMenuTrigger = element.children[2];
+      console.log(subMenuTrigger)
+
+      subMenuTrigger.addEventListener("click", function (event) {
+        event.preventDefault();
+        event.currentTarget.parentNode.classList.toggle("_is-active");
+      });
+    }
+  });
+}
+
 

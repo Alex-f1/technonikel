bd7b79a0cbf705f37872ba2e7c0b514d07e1f67d
Alex <skyriver.f1@gmail.com>
2023-03-21 16:20:47 +0500

    added article 4 page & more

--- marmelad/_blocks/content-block/content-block.js
+++ marmelad/_blocks/content-block/content-block.js
@@ -8,6 +8,7 @@ if (window.matchMedia("(max-width: 1023px)").matches) {
 }
 
 if (document.querySelectorAll(".js-content-block-sidebar").length) {
+  const sidebarBlock = document.querySelector(".js-content-block-sidebar");
   const sidebarNav = document.querySelectorAll(".js-content-block-sidebar ul li");
 
   sidebarNav.forEach(function (element, index) {
@@ -17,11 +18,16 @@ if (document.querySelectorAll(".js-content-block-sidebar").length) {
       element.appendChild(createSubMenuTrigger);
       element.children[2].classList.value = "sub-menu-trigger";
       let subMenuTrigger = element.children[2];
-      console.log(subMenuTrigger)
 
       subMenuTrigger.addEventListener("click", function (event) {
         event.preventDefault();
         event.currentTarget.parentNode.classList.toggle("_is-active");
+        // event.target.closest(".js-content-block-sidebar").classList.add("_is-show")
+        if (sidebarBlock.offsetHeight > 500) {
+          sidebarBlock.style.position = "static"
+        } else {
+          sidebarBlock.style.position =  "sticky"
+        }
       });
     }
   });
